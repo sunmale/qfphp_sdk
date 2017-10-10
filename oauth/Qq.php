f4409181612f9b22b643761576fd21a99dbd8692
晴枫 <1982127547@qq.com>
2017-10-10 13:51:13 +0800

    commit again

--- oauth/Qq.php
+++ oauth/Qq.php
@@ -43,16 +43,15 @@ class Qq   {
      * @return mixed
      */
      public   static  function  getUserInfo($code,$data){
-         $token_array =  self::getAccessToken($code,$data);
-         $user_url =  self::$getOpenIdURL."?access_token=$token_array[access_token]";
+         $access_token =  self::getAccessToken($code,$data);
+         $user_url =  self::$getOpenIdURL."?access_token=$access_token";
          $res_json  =  self::https_request($user_url);
          $res = json_decode(trim(substr($res_json, 9), " );\n"), true);
-         $user_info_url =  self::$getUserInfoURL."?access_token=$token_array[access_token]&oauth_consumer_key=$data[appid]&openid=$res[openid]";
+         $user_info_url =  self::$getUserInfoURL."?access_token=$access_token&oauth_consumer_key=$data[appid]&openid=$res[openid]";
          $user_info_json  =  self::https_request($user_info_url);
          $user_info =   json_decode($user_info_json,true);
          $user_info['openid'] = $res['openid'];
          return $user_info;
-
      }
 
 
@@ -64,16 +63,44 @@ class Qq   {
      * @throws \Exception
      */
      public static function  getAccessToken($code,$data){
-         $url =  self::$GetAccessTokenURL."?grant_type=authorization_code&client_id=$data[appid]&client_secret=$data[secret]&code=$code&redirect_uri=$data[callback_url]";
-         $token_json  =  self::https_request($url);
+         //保存文件路径
+         $path = __DIR__ . DIRECTORY_SEPARATOR .'assert'.DIRECTORY_SEPARATOR .'data';
+         if(!is_dir($path)){
+             mkdir(iconv("UTF-8", "GBK", $path), 0777, true);
+         }
+         //文件名称
+         $access_token_file =$path .DIRECTORY_SEPARATOR . 'access_token.json';
+         if(!is_file($access_token_file)){
+             self::set_php_file($access_token_file,"");
+         }
+         $res_data = json_decode(self::get_php_file($access_token_file));
+         if (empty($res_data) || $res_data->expires_in < time()) {
+             $url =  self::$GetAccessTokenURL."?grant_type=authorization_code&client_id=$data[appid]&client_secret=$data[secret]&code=$code&redirect_uri=$data[callback_url]";
+             $token_json  =  self::https_request($url);
+             parse_str($token_json, $res);
+             if(isset($res['errcode'])){
+                 throw new \Exception('获取access_toke出现错误');
+                 // return $res;
+             }
+             $access_token = $res['access_token'];
+             if ($access_token) {
+                 $content = new \stdClass();
+                 $content->access_token = $access_token;
+                 $content->expires_in = time() + $res['expires_in'];
+                 self::set_php_file($access_token_file,json_encode($content));
+             }
+         } else {
+             $access_token = $res_data->access_token;
+         }
+         return $access_token;
+     /*    $token_json  =  self::https_request($url);
          parse_str($token_json, $token_array);
          if ($token_array['access_token'] && $token_array['expires_in']) {
              return $token_array;
          }else{
              throw  new \Exception('qq快捷登录获取accessToken失败');
-         }
+         }*/
      }
-
 
 
     /**
@@ -85,6 +112,7 @@ class Qq   {
          $url =  self::$GetRequestCodeURL."?client_id=$data[appid]&redirect_uri=$data[callback_url]&response_type=code&scope=get_user_info&state=1#wechat_redirect";
          return $url ;
      }
+
 
 
     /**
@@ -107,6 +135,22 @@ class Qq   {
         $output = curl_exec($curl);
         curl_close($curl);
         return $output;
+    }
+
+
+
+
+
+    //获取文件内容
+    public  static function get_php_file($filename) {
+        return  trim(file_get_contents($filename));
+    }
+
+
+
+    //写入文件内容
+    public  static function set_php_file($filename, $content) {
+        file_put_contents($filename, $content);
     }
 
 
